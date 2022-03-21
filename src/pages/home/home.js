b7649e3d92e06feeb19c82debbfb385803e1c050
Mohamed Farhat <mfarhat@umbrella.ly>
2022-03-21 15:43:35 +0200

    change

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -238,132 +238,93 @@ const Home = (props) => {
                         </h4>
                         {/* </Alert> */}
                     </Col>
-                    <Col md={4}>
-                        <ListGroup.Item
-                            as="li"
-                            className="d-flex justify-content-between align-items-start"
-                        >
-                            <div className="ms-2 me-auto">
-                                <div className="fw-bold">رقم الطلبية:</div>
-                                {orderDetails?.OrderDetails?.RefrenceNo ??
-                                    'لايوجد'}
-                            </div>
-                        </ListGroup.Item>
-                        <ListGroup.Item
-                            as="li"
-                            className="d-flex justify-content-between align-items-start"
-                        >
-                            <div className="ms-2 me-auto">
-                                <div className="fw-bold">تاريخ الإنشاء:</div>
-                                {moment
-                                    .utc(orderDetails?.OrderDetails?.CreateDate)
-                                    .local()
-                                    .format('DD/MM/YYYY HH:mm A')}
-                            </div>
-                        </ListGroup.Item>
-                    </Col>
-                    <Col md={'4'}>
-                        <ListGroup as="ol">
-                            <ListGroup.Item
-                                as="li"
-                                className="d-flex justify-content-between align-items-start"
-                            >
-                                <div className="ms-2 me-auto">
-                                    <div className="fw-bold">وصف الطلبية:</div>
-                                    {orderDetails?.OrderDetails?.Description ??
-                                        'لايوجد'}
-                                </div>
-                            </ListGroup.Item>
-                        </ListGroup>
-                        <ListGroup.Item
-                            variant="success"
-                            as="li"
-                            className="d-flex justify-content-between align-items-start"
-                        >
-                            <div
-                                style={{
-                                    fontWeight: '600',
-                                }}
-                                className="ms-2 me-auto"
-                            >
-                                <div className="fw-bold">القيمة الكلية:</div>
-                                {orderDetails?.OrderDetails?.TotalAmount ? (
-                                    <span>
-                                        {orderDetails.OrderDetails.TotalAmount}
-                                        &nbsp;دينار ليبي
-                                    </span>
-                                ) : (
-                                    'لايوجد'
-                                )}
-                            </div>
-                        </ListGroup.Item>
-                    </Col>
-                    <Col md={'4'}>
-                        <ListGroup as="ol">
-                            <ListGroup.Item
-                                as="li"
-                                className="d-flex justify-content-between align-items-start"
-                            >
-                                <div className="ms-2 me-auto">
-                                    <div className="fw-bold">اسم المحل:</div>
-                                    {orderDetails?.OrderDetails?.ShopName ? (
-                                        <span>
-                                            <a
-                                                href={`tel:${orderDetails?.OrderDetails?.ShopTelNo1}`}
-                                            >
+                    <Col xs={12}>
+                        <Row>
+                            <Col xs={6} md={4}>
+                                <ListGroup.Item
+                                    style={{ border: 0 }}
+                                    as="li"
+                                    className="d-flex justify-content-between align-items-start"
+                                >
+                                    <div className="ms-2 me-auto">
+                                        <div className="fw-bold">
+                                            رقم الطلبية:
+                                        </div>
+                                        {orderDetails?.OrderDetails
+                                            ?.RefrenceNo ?? 'لايوجد'}
+                                    </div>
+                                </ListGroup.Item>
+                            </Col>
+                            <Col xs={6} md={4}>
+                                <ListGroup.Item
+                                    style={{ border: 0 }}
+                                    as="li"
+                                    className="d-flex justify-content-between align-items-start"
+                                >
+                                    <div className="ms-2 me-auto">
+                                        <div className="fw-bold">
+                                            تاريخ الإنشاء:
+                                        </div>
+                                        {moment
+                                            .utc(
+                                                orderDetails?.OrderDetails
+                                                    ?.CreateDate
+                                            )
+                                            .local()
+                                            .format('DD/MM/YYYY HH:mm A')}
+                                    </div>
+                                </ListGroup.Item>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={12} md={4}>
+                                <ListGroup.Item
+                                    style={{ border: 0 }}
+                                    as="li"
+                                    className="d-flex justify-content-between align-items-start"
+                                >
+                                    <div className="ms-2 me-auto">
+                                        <div className="fw-bold">
+                                            وصف الطلبية:
+                                        </div>
+                                        {orderDetails?.OrderDetails
+                                            ?.Description ?? 'لايوجد'}
+                                    </div>
+                                </ListGroup.Item>
+                            </Col>
+                            <Col xs={12} md={4}>
+                                <ListGroup.Item
+                                    style={{ border: 0 }}
+                                    variant="danger"
+                                    as="li"
+                                    className="d-flex justify-content-between align-items-start"
+                                >
+                                    <div
+                                        style={{
+                                            fontWeight: '600',
+                                        }}
+                                        className="ms-2 me-auto"
+                                    >
+                                        <div className="fw-bold">
+                                            القيمة الكلية:
+                                        </div>
+                                        {orderDetails?.OrderDetails
+                                            ?.TotalAmount ? (
+                                            <span>
                                                 {
                                                     orderDetails.OrderDetails
-                                                        .ShopName
+                                                        .TotalAmount
                                                 }
-                                                &nbsp; &nbsp;
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="16"
-                                                    height="16"
-                                                    fill="blue"
-                                                    className="bi bi-telephone-fill"
-                                                    viewBox="0 0 16 16"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
-                                                    />
-                                                </svg>
-                                            </a>
-                                        </span>
-                                    ) : (
-                                        'لايوجد'
-                                    )}
-                                </div>
-                            </ListGroup.Item>
-                            {/* <ListGroup.Item
-                                as="li"
-                                className="d-flex justify-content-between align-items-start"
-                            >
-                                <div className="ms-2 me-auto">
-                                    <div className="fw-bold">رقم المحل:</div>
-                                    {orderDetails?.OrderDetails?.ShopTelNo ??
-                                        'لايوجد'}
-                                </div>
-                            </ListGroup.Item> */}
-                        </ListGroup>
-                    </Col>
-                    {/* <Col md={'4'}>
-                        <ListGroup as="ol">
-                            <ListGroup.Item
-                                as="li"
-                                className="d-flex justify-content-between align-items-start"
-                            >
-                                <div className="ms-2 me-auto">
-                                    <div className="fw-bold">
-                                        رقم هاتف الزبون:
+                                                &nbsp;دينار ليبي
+                                            </span>
+                                        ) : (
+                                            'لايوجد'
+                                        )}
                                     </div>
-                                    {orderDetails?.OrderDetails?.ClientTelNo ??
-                                        'لايوجد'}
-                                </div>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Col> */}
+                                </ListGroup.Item>
+                            </Col>
+                        </Row>
+                    </Col>
                 </Row>
             )}
             <Row>
